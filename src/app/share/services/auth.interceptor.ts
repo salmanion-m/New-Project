c2663235db886179d0632339fdce1bc0a0ff3291
SN <sn.sarvinm@gmail.com>
2023-01-01 17:51:36 +0330

    edited auth.interceptor

--- src/app/share/services/auth.interceptor.ts
+++ src/app/share/services/auth.interceptor.ts
@@ -3,11 +3,10 @@ import {
   HttpRequest,
   HttpHandler,
   HttpEvent,
-  HttpInterceptor, HttpErrorResponse, HttpHeaders
+  HttpInterceptor
 } from '@angular/common/http';
-import {catchError, Observable, throwError} from 'rxjs';
+import {Observable} from 'rxjs';
 import {AuthService} from "../../auth";
-import {environment} from "../../../environments/environment";
 
 @Injectable()
 
@@ -17,32 +16,6 @@ export class AuthInterceptor implements HttpInterceptor {
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
 
-      // request = request.clone({
-      // //   headers: request.headers.set('authorization' , 'token')
-      // // })
-      // //
-      // const token = this.authService.getAuthToken();
-      // if(token){
-      //   request = request.clone({
-      //     setHeaders: {Authorization: `Authorization token ${token}`}
-      //   });
-      // }
-      //
-      // // return next.handle(request);
-      // return next.handle(request).pipe(
-      //   catchError((err) => {
-      //     if (err instanceof HttpErrorResponse) {
-      //       if (err.status === 401) {
-      //         // redirect user to the logout page
-      //       }
-      //     }
-      //     return throwError(err);
-      //   })
-      // )
-
-
-    // return  next.handle(this.addAuthToken(request));
-    //
     request = request.clone({
       setHeaders: {
         'Content-Type' : 'application/json; charset=utf-8',
@@ -53,12 +26,5 @@ export class AuthInterceptor implements HttpInterceptor {
     return next.handle(request);
   }
 
-  // addAuthToken(request: HttpRequest<any>){
-  //   const token = this.authService.getAuthToken();
-  //
-  //   return request.clone({
-  //     setHeaders:{ Authorization: `Basic ${token}`}
-  //   })
-  // }
 
 }
