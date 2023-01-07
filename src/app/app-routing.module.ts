33571887e8300c67ede568a8a87316848ecb165d
SN <sn.sarvinm@gmail.com>
2023-01-07 10:13:00 +0330

    edited app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,19 +1,23 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 import {FundTableComponent} from "./fund";
 import {IndustryTableComponent} from "./industry/components";
 import {LetterTypeTableComponent} from "./letter-type";
 import {InstrumentTableComponent} from "./instrument/components";
-import {DailyPriceTableComponent} from "./daily";
 
 const routes: Routes = [
+  {path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+  {path: 'dashbord', loadChildren: () => import('./dashbord/dashbord.module').then(m => m.DashbordModule)},
+  {path: 'day' , loadChildren: ()=> import('./daily-price/daily-price.module').then(m=> m.DailyPriceModule)},
   {path: 'fund', loadChildren: () => import('./fund/fund.module').then(m => m.FundModule)},
   // {path: 'fund',component: FundTableComponent},
-  {path: 'industry',component: IndustryTableComponent},
-  {path: 'letter',component: LetterTypeTableComponent},
-  {path: 'inst' ,component: InstrumentTableComponent},
-  {path: 'daily' , component: DailyPriceTableComponent},
-  {path: 'auth' , loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)},
+  {path: 'industry', loadChildren: () => import('./industry/industry.module').then(m => m.IndustryModule)},
+  // {path: 'industry', component: IndustryTableComponent},
+  {path: 'letter', loadChildren: () => import('./letter-type/letter-type.module').then(m => m.LetterTypeModule)},
+  // {path: 'letter', component: LetterTypeTableComponent},
+  {path: 'instrument', loadChildren: () => import('./instrument/instrument.module').then(m => m.InstrumentModule)},
+  // {path: 'inst', component: InstrumentTableComponent},
+
 
 ];
 
@@ -21,4 +25,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
